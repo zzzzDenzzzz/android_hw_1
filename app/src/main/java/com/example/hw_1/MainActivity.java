1ddf41f2122c301d7dc27c71597f0bea67ecc534
zzzzDenzzzz <12den24@gmail.com>
2024-07-23 00:00:27 +0300

    init

---
+++ app/src/main/java/com/example/hw_1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.hw_1;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(view -> textView.setText(String.valueOf(editText.getText()).toUpperCase()));
+    }
+}
